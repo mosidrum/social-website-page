daeff036f23dd7eed818dca0333970378d5fb0ee
Tomilola662 <tomilola243@gmail.com>
2025-09-17 21:36:57 +0100

    added content box to hero section

--- src/components/Hero-Section/Hero.jsx
+++ src/components/Hero-Section/Hero.jsx
@@ -1,11 +1,18 @@
 import './Hero.css';
 import protrait from './images/protrait.jpg';
 import Avatar from './Avatar.jsx';
-
+import ContentBox from './ContentBox.jsx';
 function Hero() {
   return (
     <div className="container">
       <Avatar photo={protrait} text="PRO" />
+      <ContentBox
+        title="Tomilola Valour"
+        profession="Front-end Web Developer, & Software Engineer"
+        about="Passionate about creating beautiful digital experiences
+         and sharing creative inspiration with the world. Follow me across
+          platforms for design tips, tutorials, and behind-the-scenes content."
+      />
     </div>
   );
 }
