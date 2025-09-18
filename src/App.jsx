8f3139566a76e59447ad064d8b19dd8455c25451
rollings-dev12 <igonirollings30@gmail.com>
2025-09-18 10:48:05 -0700

    added contact me

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
-
+import Contact from './components/Contact-me/contact.jsx';
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      Hello
+      <Contact />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Contact-me/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header-contaner">
+      <h1>Connect With Me</h1>
+      <p>
+        Follow me across different platforms for unique content, tutorials, and
+        <br></br>
+        creative inspiration. Each platform offers something special!
+      </p>
+    </div>
+  );
+};
+
+export default Header;
