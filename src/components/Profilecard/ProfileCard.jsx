43294f4a06dbd7a6360be411769b22c5a3b27358
code-with-Atomic <ikehchibuikevincent@gmail.com>
2025-09-18 15:23:31 -0700

    add

--- src/components/Profilecard/ProfileCard.jsx
+++ src/components/Profilecard/ProfileCard.jsx
@@ -1,21 +1,26 @@
 
-import "./ProfileCard.css"
+import './ProfileCard.css';
 
-const heading = <h1 className="profile-heading">What people say <p>Feedback from followers,students, and collaborators</p></h1>;
+const heading = (
+  <h1 className="profile-heading">
+    What people say
+    <p>Feedback from followers, students, and collaborators</p>
+  </h1>
+);
 
 const ProfileCard = (props) => {
- return (
-         <div className="profile-container">
-        <div className="profile-box">       
-        <img src={props.image} alt=''/>
-        <div className='profile-description'>{props.description}
-        </div>
-        <div className='name'>{props.name}</div>
-        <div className='profile-Job'>{props.job}</div>
-        <div className='profile-Title'>{props.Title}</div>
-        </div>
-   </div>
-    )
-}
-export  {heading};
-export default ProfileCard
+  return (
+    <div className="profile-container">
+      <div className="profile-box">
+        <img src={props.image} alt="" />
+        <div className="profile-description">{props.description}</div>
+        <div className="name">{props.name}</div>
+        <div className="profile-Job">{props.job}</div>
+        <div className="profile-Title">{props.Title}</div>
+      </div>
+    </div>
+  );
+};
+
+export { heading };
+export default ProfileCard;
