e9d05b7b5407cc38283043ffc6e8829cca357f46
Tomilola662 <tomilola243@gmail.com>
2025-09-18 13:08:18 +0100

    Added socials section and made requested changes

--- src/components/Hero-Section/Hero.jsx
+++ src/components/Hero-Section/Hero.jsx
@@ -2,6 +2,8 @@ import './Hero.css';
 import protrait from './images/protrait.jpg';
 import Avatar from './Avatar.jsx';
 import ContentBox from './ContentBox.jsx';
+import SocialCard from './SocialCard.jsx';
+
 function Hero() {
   return (
     <div className="container">
@@ -13,6 +15,12 @@ function Hero() {
          and sharing creative inspiration with the world. Follow me across
           platforms for design tips, tutorials, and behind-the-scenes content."
       />
+      <div className="socials-box">
+        <SocialCard value="2M+" text="Content Views" />
+        <SocialCard value="50k+" text="Students Taught" />
+        <SocialCard value="100+" text="Brands Collaborated" />
+        <SocialCard value="4.7" text="Average Rating" icon="fa-solid fa-star" />
+      </div>
     </div>
   );
 }
