706e67fad3c3239708f1824d8566a39dfb4b5ac1
tial0011 <taiwoalex34@gmail.comgit config --global user.name tial0011git config --global user.email taiwoalex34@gmail.com>
2025-09-19 12:11:42 +0100

    i reolved conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
 
 import FC from './components/FeaturedContent1.jsx';
 import Hero from './components/Hero-Section/Hero.jsx';
-=======
 import Hero from './components/Hero-Section/Hero.jsx'
->>>>>>> eda8b4437267b4510d38b9d28af6f065d8d6f0f1
 function App() {
   return (
     <div>
