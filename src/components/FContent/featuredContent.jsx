ca00448c497b1f6870affaca2b1738f8dcb69fcf
tial0011 <taiwoalex34@gmail.comgit config --global user.name tial0011git config --global user.email taiwoalex34@gmail.com>
2025-09-19 14:54:24 +0100

    this is the FeaturedContent

---
+++ src/components/FContent/featuredContent.jsx
@@ -0,0 +1,19 @@
+import './featuredContentstyle.css'
+function FeaturedContent(props) {
+  return (
+    <div className="card">
+      <div className="imgContainer">
+        <img className="cardImg" src={props.image}></img>
+      </div>
+      <div className="social">
+        <span className="media">{props.socialMedia}</span>
+        <span className="views">{props.views}</span>
+      </div>
+      <div className="content">
+        <h3>{props.contentHeading}</h3>
+        <p>{props.content}</p>
+      </div>
+    </div>
+  )
+}
+export default FeaturedContent
