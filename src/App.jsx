3f5952ddfa5b47d5ce3245ed9d5a97356fc67bce
tial0011 <taiwoalex34@gmail.comgit config --global user.name tial0011git config --global user.email taiwoalex34@gmail.com>
2025-09-19 11:56:04 +0100

    i added featuredContent

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 
 import FC from './components/FeaturedContent1.jsx';
-
-function App() {
-  return (
-    <>
-      <FC/>;
-    </>
 import Hero from './components/Hero-Section/Hero.jsx';
 function App() {
   return (
     <div>
       <Hero />
+      <FC/>
     </div>
   );
 }
