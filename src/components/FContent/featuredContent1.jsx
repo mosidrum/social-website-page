aa3733d04081f436fb7796a29b23f9d41af5f628
tial0011 <taiwoalex34@gmail.comgit config --global user.name tial0011git config --global user.email taiwoalex34@gmail.com>
2025-09-22 12:42:18 +0100

    i changed the FC repitition to an  array

--- src/components/FContent/featuredContent1.jsx
+++ src/components/FContent/featuredContent1.jsx
@@ -3,36 +3,50 @@ import FeaturedContent from './featuredContent.jsx'
 import img from './FCimages/image.jpg'
 import imgone from './FCimages/image0.jpg'
 import imgtwo from './FCimages/image1.jpg'
+
+const featuredItems = [
+  {
+    image: img,
+    socialMedia: 'Youtube',
+    views: '125k views',
+    contentHeading: 'Design System Mastery',
+    content: 'Complete guide to building scalable design systems'
+  },
+  {
+    image: imgone,
+    socialMedia: 'Instagram',
+    views: '89k views',
+    contentHeading: 'Brand identity workshop',
+    content: 'Steps-by-steps branding process for startups'
+  },
+  {
+    image: imgtwo,
+    socialMedia: 'Tiktok',
+    views: '287k views',
+    contentHeading: 'UI Animation Techniques',
+    content: 'Creating micro-interactions that delight users'
+  }
+]
+
 function FC() {
   return (
     <div className="main-container">
       <FeaturedContentHeading />
 
       <div className="cardsContainer">
-        <FeaturedContent
-          image={img}
-          socialMedia="Youtube"
-          views="125k views"
-          contentHeading="Design System Mastery"
-          content="Complete guide to building scalable design systems"
-        />
-        <FeaturedContent
-          image={imgone}
-          socialMedia="Instagram"
-          views="89k views"
-          contentHeading="Brand identity workshop"
-          content="Steps-by-steps branding process for startups"
-        />
-
-        <FeaturedContent
-          image={imgtwo}
-          socialMedia="Tiktok"
-          views="287k views"
-          contentHeading="UI Animation Techniques"
-          content="Creating micro-interactions that delight users"
-        />
+        {featuredItems.map((item, index) => (
+          <FeaturedContent
+            key={index}
+            image={item.image}
+            socialMedia={item.socialMedia}
+            views={item.views}
+            contentHeading={item.contentHeading}
+            content={item.content}
+          />
+        ))}
       </div>
     </div>
   )
 }
+
 export default FC
