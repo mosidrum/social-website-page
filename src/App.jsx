245e8de913376c852cc8e27e5da568dca52703c0
code-with-Atomic <Ikehchibuikevincent@gmail.com>
2025-09-22 22:47:04 -0700

    What people say
    * error fix
    
    * Added All the profile card content
    
    * resolved lint
    
    * using map
    
    * resolved eslint
    
    * Resolve merge conflict in App.jsx (kept both Hero and Contact)
    
    * resolved netilify
    
    * ammended app.jsx
    
    * added quote,background color
    
    * resolved elint

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import './App.css'
+import Card from './components/what-people-say/Card.jsx'
 import Hero from './components/Hero-Section/Hero.jsx'
 import Contact from './components/Contact-me/contact.jsx'
+
 function App() {
   return (
-    <div>
+    <>
       <Hero />
       <Contact />
-    </div>
+      <Card />
+    </>
   )
 }
 
