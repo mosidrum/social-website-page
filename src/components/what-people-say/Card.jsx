47d126541ae7934779bed48d36f1fadc5c82e5d3
code-with-Atomic <ikehchibuikevincent@gmail.com>
2025-09-22 18:42:49 -0700

    resolved elint

--- src/components/what-people-say/Card.jsx
+++ src/components/what-people-say/Card.jsx
@@ -7,26 +7,26 @@ const Card = () => {
       image: '/bro1.jpg',
       name: 'Vincent',
       description:
-        '"Vincent\'s design tutorials transformed our team\'s approach to visual content. His clear explanations and practical tips are invaluable."',
+        "Vincent's design tutorials transformed our team's approach to visual content. His clear explanations and practical tips are invaluable.",
       job: 'Marketing Designer',
-      Title: 'Tech Marketer',
+      Title: 'Tech Marketer'
     },
     {
       image: '/bro2.jpg',
       name: 'Michael',
       description:
-        '"Following Vincent across platforms has elevated my design skills tremendously. His consistency is high-quality and inspiring."',
+        'Following Vincent across platforms has elevated my design skills tremendously. His consistency is high-quality and inspiring.',
       job: 'Graphics Designer',
-      Title: 'Tech Graphics',
+      Title: 'Tech Graphics'
     },
     {
       image: '/bro4.jpg',
       name: 'Enoch',
       description:
-        '"Vincent\'s social media presence is authentic and educational. His behind-the-scenes content gives real insight into the creative process."',
+        "Vincent's social media presence is authentic and educational. His behind-the-scenes content gives real insight into the creative process.",
       job: 'Fashion Designer',
-      Title: 'Tech Fashion',
-    },
+      Title: 'Tech Fashion'
+    }
   ]
 
   return (
